5ad1ee55df2001fb0846e0d0a5bc875caaf4d38a
famim2002 <famimhayatyash@gmail.com>
2025-06-10 22:54:00 +0600

    styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,9 @@ const Contact = () => {
     <section>
       <div className="container">
         <div className="h-dvh flex  mx-auto gap-5 justify-center items-center">
-          <h1 className="text-6xl  text-white  font-headerFont items-center">
+          <h1 className="text-6xl  text-red-600 font-semibold  font-headerFont items-center">
             this is Contact page
+            
           </h1>
         </div>
       </div>
