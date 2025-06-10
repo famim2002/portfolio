8e86002d59c8f3922f95914071929d194ec411cd
famim2002 <famimhayatyash@gmail.com>
2025-06-10 22:29:34 +0600

    styling

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Banner = () => {
-  return <section></section>;
+  return (
+    <section>
+      <h2 className='flex justify-center text-6xl text-white'>this is banner</h2>
+    </section>
+  );
 }
 
 export default Banner
