c83e6638668914aa9fe3f2f4ac766b6b0f53cad4
famim2002 <famimhayatyash@gmail.com>
2025-06-15 13:12:22 +0600

    social link  bar styling

--- src/components/common-components/SocialBar.jsx
+++ src/components/common-components/SocialBar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import {
   FaFacebookF,
 
-  FaTwitter,
+
   FaInstagram,
+  FaGithub,
  
 } from "react-icons/fa";
 
@@ -17,32 +18,26 @@ const SocialLinks = () => {
         rel="noopener noreferrer"
         className="group bg-white w-[90px] h-[90px] rounded-[28%] shadow-md flex items-center justify-center transition duration-300 hover:bg-[#4267B2]"
       >
-        <FaFacebookF className="text-[#4267B2] text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-125" />
+        <FaFacebookF className="text-[#4267B2]  w-full h-full text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-115" />
       </a>
 
-  
-
-      {/* Twitter */}
       <a
         href="https://twitter.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="group bg-white w-[90px] h-[90px] rounded-[28%] shadow-md flex items-center justify-center transition duration-300 hover:bg-[#1DA1F2]"
+        className="group bg-white w-[90px] h-[90px] rounded-[28%] shadow-md flex items-center justify-center transition duration-300 hover:bg-[#1d1d1d]"
       >
-        <FaTwitter className="text-[#1DA1F2] text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-125" />
+        <FaGithub className="text-[#1d1d1d]  w-full h-full text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-115" />
       </a>
 
-      {/* Instagram */}
       <a
         href="https://instagram.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="group bg-white w-[90px] h-[90px] rounded-[28%] shadow-md flex items-center justify-center transition duration-300 hover:bg-gradient-to-br from-[#fdf497] via-[#fd5949] to-[#285AEB]"
+        className="group bg-white w-[90px] h-[90px]  rounded-[28%] shadow-md flex items-center justify-center transition duration-300 hover:bg-gradient-to-br from-[#fdf497] via-[#fd5949] to-[#285AEB]"
       >
-        <FaInstagram  className=" bg-clip-text bg-gradient-to-br from-[#fdf497] via-[#fd5949] to-[#285AEB] text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-125" />
+        <FaInstagram className="text-pink-700 w-full h-full bg-clip-text  bg-gradient-to-br from-[#fdf497] via-[#fd5949] to-[#285AEB] text-3xl transition-all duration-300 group-hover:text-white group-hover:scale-125" />
       </a>
-
-    
     </div>
   );
 };
