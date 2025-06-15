7de85357164e54991db4cb3cef1a2230d76e54e8
famim2002 <famimhayatyash@gmail.com>
2025-06-15 16:58:33 +0600

    styling

--- src/components/home/ContactMe.jsx
+++ src/components/home/ContactMe.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import WorldMapComponent from "./home-components/WorldMap";
+
 
 const ContactMe = () => {
   return (
     <section className="px-3 h-dvh overflow-hidden">
       <div className="container">
-        
-          <div className="h-full">
-            <WorldMapComponent />
-         <social
        
-          </div>
-        </div>
-      
+      </div>
     </section>
   );
 };
