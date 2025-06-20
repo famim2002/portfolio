54b76f04b515420b5ce5aa082fe2a903fb8cb048
famim2002 <famimhayatyash@gmail.com>
2025-06-20 20:44:32 +0600

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
+import { BrowserRouter } from 'react-router'
+import Home from './pages/Home'
+import Error404 from './pages/Error404'
+import About from './pages/About'
+import Services from './pages/Services'
+import Demo from './pages/Demo'
+import Contact from './pages/Contact'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/demo" element={<Demo />} />
+              <Route path="/contact" element={<Contact />} />
+              
+            </Route>
+          </Route>
+          <Route path="*" element={<Error404 />}></Route>
+    
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/pages/Demo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Demo = () => {
+  return <div>Projects</div>;
+};
+
+export default Demo;
